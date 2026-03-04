d5431f7d52b33695632e2a76035251c8775dfdd5
rachel-sony <rachel.chan@sony.com>
2026-03-04 12:26:34 +0900

    pyproject formatting

--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
+import os
+
+# Helper to read README
+def read_file(filename):
+    if os.path.exists(filename):
+        with open(filename, encoding='utf-8') as f:
+            return f.read()
+    return ""
 
 setup(
     name="dornet",
     version="2026.03.04",
     author="yanzq95",
     description="DORNet: A Degradation Oriented and Regularized Network for Blind Depth Super-Resolution (CVPR 2025)",
-    long_description=open("dornet/README.md").read(),
+    long_description=read_file("dornet/README.md"),
     long_description_content_type="text/markdown",
-    license="file: dornet/LICENSE",
+    license=read_file("dornet/LICENSE"),
     packages=find_packages(),
     python_requires=">=3.10",
+
     install_requires=[
         "torch>=2.1.0",
         "numpy<2.0",
@@ -18,10 +27,14 @@ setup(
         "Pillow>=10.0.1",
         "tqdm>=4.65.0",
         "scikit-image>=0.21.0",
-        "mmcv-full==1.7.2",
         "openmim>=0.3.9",
+        "mmcv>=2.1.0",
     ],
-    dependency_links=["https://download.openmmlab.com/mmcv/dist/cu117/torch2.0.0/index.html"],
+
+    dependency_links=[
+        "https://download.openmmlab.com/mmcv/dist/cu118/torch2.1.0/index.html"
+    ],
+
     include_package_data=True,
     package_data={
         "dornet": ["checkpoints/*.pth"],
